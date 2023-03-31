eaaa2e872ac1b276fb57666aa9eb45268f520e3b
mathGoOli <matheuss456@hotmail.com>
2023-03-31 13:57:06 -0300

    falta o modal

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,11 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function make_modal(){
+    const button = document.querySelector('#btnSubmit')
+    const modal = document.querySelector('.modal__controller')
+    
+    button.addEventListener('click', () =>{
+        modal.showModal()
+    })
+}
+
+make_modal()

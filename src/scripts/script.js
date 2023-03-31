b961187e3d5286f3ea5241f817d83e1fc4422a26
mathGoOli <matheuss456@hotmail.com>
2023-03-31 18:25:20 -0300

    finished

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,8 +3,19 @@ function make_modal(){
     const button = document.querySelector('#btnSubmit')
     const modal = document.querySelector('.modal__controller')
     
+    
     button.addEventListener('click', () =>{
         modal.showModal()
+        modalClose()
+    })
+}
+
+function modalClose(){
+    const close = document.querySelector('.modal__close')
+    const modal = document.querySelector('.modal__controller')
+
+    close.addEventListener('click', () =>{
+        modal.close()
     })
 }
 
